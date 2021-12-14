d74f0f24fd9b7f832a2d8fba9b4932ca3fc6c62c
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-12-14 19:45:59 -0300

    Trying to upload files to MongoDB, will attempt to deploy the app before that though

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const mongoose = require('mongoose');
 require('dotenv').config()
+const upload = multer({ dest: 'uploads/' })
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 var app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {   
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
@@ -18,3 +27,11 @@ const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'),(req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
